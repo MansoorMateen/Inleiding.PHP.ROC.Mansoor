e82c1d50258985858d4f025634e884fb2b469f12
Mansoor <mansoor1999@live.com>
2021-03-31 14:22:18 +0200

    opdr3 begin

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 </head>
 <body>
 <a href="H01/index.php">HO1</a>
-<a href="H02/zin1.php">HO2</a>
-<a href="H03/fotos.php">HO3</a>
+<a href="H02/index.php">HO2</a>
+<a href="H03/index.php">HO3</a>
 
 
 
